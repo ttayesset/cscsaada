2ddb6d0ffc16394f508db78d03e3ca2338daab60
ttayesset <121930520+ttayesset@users.noreply.github.com>
2024-04-29 13:13:14 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
-import requests
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask Github!'
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
